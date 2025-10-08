da0eaaf524f1a03f2360eb0a032d99b155f74beb
tushar <tusharkokane366@gmail.com>
2025-10-08 23:18:06 +0530

    feat: Add the user modal and its role

---
+++ service/src/main/java/com/user/service/modal/User.java
@@ -0,0 +1,32 @@
+package com.user.service.modal;
+
+import com.user.service.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+}
+
+
+
